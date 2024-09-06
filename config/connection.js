c4281780bf3b170489457f91c56b453a9c58cf2d
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-06 19:27:11 -0400

    populated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,14 @@
-const { connect, connection} = require
+const { connect, connection} = require('mongoose');
+
+// Connection string for MongoDB
+
+const connectionString = 'mongodb://127.0.0.1:27017/socialDB';
+
+// Connect to MongoDB using the connection string\
+connect( connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Export the connection
+module.exports = connection;
